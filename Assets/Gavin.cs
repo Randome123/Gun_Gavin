af3828631492c29155b2f5e77198c69c0dc2d199
Jordan Igori <jigori@gmail.com>
2017-05-08 16:18:35 -0700

    Player, walking, and minor physics
    I added a sprite (even though he looks the same throughout), made him
    walk, and made sure he does not go through the ground

---
+++ Assets/Gavin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gavin : MonoBehaviour {
+
+	public float FloorHeight;
+	public float Speed;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Stops player from falling through floor
+		if (rb.position.y <= FloorHeight) rb.MovePosition (new Vector2 (rb.position.x, FloorHeight));
+
+		// Allows player to move
+		float hSpeed = Input.GetAxis("Horizontal") * Speed;
+
+		rb.AddForce (new Vector2 (hSpeed, 0));
+	}
+}
